b925cedd71f9ab10b870d4271259d3673b3c25d7
Noah Huetter <noahhuetter@gmail.com>
2018-03-16 12:05:31 +0100

    maximum buffer check program

--- sw/uft/tests/checkopts.c
+++ sw/uft/tests/checkopts.c
@@ -67,7 +67,7 @@ struct sock_opts {
 	{ "SO_REUSEPORT",		0,			0,				NULL },
 #endif
 	{ "SO_TYPE",			SOL_SOCKET,	SO_TYPE,		sock_str_int },
-	{ "SO_USELOOPBACK",		SOL_SOCKET,	SO_USELOOPBACK,	sock_str_flag },
+	// { "SO_USELOOPBACK",		SOL_SOCKET,	SO_USELOOPBACK,	sock_str_flag },
 	{ "IP_TOS",				IPPROTO_IP,	IP_TOS,			sock_str_int },
 	{ "IP_TTL",				IPPROTO_IP,	IP_TTL,			sock_str_int },
 #ifdef	IPV6_DONTFRAG
