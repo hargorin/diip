d475456c4dbcccbc1e38dc996a36d496a1e775d8
Noah Huetter <noahhuetter@gmail.com>
2018-05-09 10:15:10 +0200

    Add Xilinx state machine example

---
+++ fpga/hls/projects/state_example/project.tcl
@@ -0,0 +1,20 @@
+# @Author: Noah
+# @Date:   2018-04-27 14:46:24
+# @Last Modified by:   Noah
+# @Last Modified time: 2018-04-27 16:12:13
+
+# Set the function that will be implemented
+set proj_top_function hlsExample
+
+# Set the clock period in [ns]
+set proj_clk_period 10
+
+# Set the clock period in [ns]
+set proj_part_name "xc7a200tfbg676-2"
+
+# Settings of IP generation
+set proj_vendor "ime"
+set proj_library "diip"
+set proj_version "0.1" 
+set proj_desc "State example" 
+set proj_display_name "State example"
