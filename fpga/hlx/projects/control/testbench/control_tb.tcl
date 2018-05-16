8ae84dade5065fd08ccb102dabb3e07b51e728cd
Noah <noahhuetter@gmail.com>
2018-05-16 15:14:30 +0200

    Controller TCL testbench

--- fpga/hlx/projects/control/testbench/control_tb.tcl
+++ fpga/hlx/projects/control/testbench/control_tb.tcl
@@ -9,15 +9,25 @@ proc connect {} {
   connect_hw_server
   open_hw_target
 
-  set_property PROGRAM.FILE {build/projects/control.runs/impl_1/system_wrapper.bit} [get_hw_devices xc7a200t_0]
-  set_property PROBES.FILE {build/projects/control.runs/impl_1/system_wrapper.ltx} [get_hw_devices xc7a200t_0]
-  set_property FULL_PROBES.FILE {build/projects/control.runs/impl_1/system_wrapper.ltx} [get_hw_devices xc7a200t_0]
+  set_property PROGRAM.FILE {../../../build/projects/control.runs/impl_1/system_wrapper.bit} [get_hw_devices xc7a200t_0]
+  set_property PROBES.FILE {../../../build/projects/control.runs/impl_1/system_wrapper.ltx} [get_hw_devices xc7a200t_0]
+  set_property FULL_PROBES.FILE {../../../build/projects/control.runs/impl_1/system_wrapper.ltx} [get_hw_devices xc7a200t_0]
   current_hw_device [get_hw_devices xc7a200t_0]
   refresh_hw_device [lindex [get_hw_devices xc7a200t_0] 0]
 }
 # connect_xc7a200t
 proc disconnect {} {
   disconnect_hw_server
+}
+
+proc anykey {{msg "Hit any key: "}} {
+    set stty_settings [exec stty -g]
+    exec stty raw -echo
+    puts -nonewline $msg
+    flush stdout
+    read stdin 1
+    exec stty $stty_settings
+    puts ""
 }
 
 #############################################
@@ -30,9 +40,22 @@ proc flash {} {
 # --------
 #   main
 # --------
-# source memdump.tcl
+source memdump.tcl
 open_hw
 connect
 
+# Write data to image input base
+writeto data.bin 0
+
+# Push SW5 to run calculation
+puts "Push SW5 to start processing"
+puts "Press enter when done"
+anykey
+
+# Dump output data
+dump out.bin 400 144
+
+# Check data
+puts "Check output data in out.bin"
 
 # close_hw
