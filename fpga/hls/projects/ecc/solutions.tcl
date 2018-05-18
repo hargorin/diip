fe8129454cc1d1ccf70a80d3908b67055a9da103
hargorin <janstocker@gmx.ch>
2018-05-18 14:35:45 +0200

    sol0: Mean and Variance is running

---
+++ fpga/hls/projects/ecc/solutions.tcl
@@ -0,0 +1,16 @@
+# @Author: Jan Stocker
+# @Date:   2018-04-27 14:46:24
+# @Last Modified by:   Jan Stocker
+# @Last Modified time: 2018-04-27 16:12:13
+
+############################################################
+## Solutions
+############################################################
+
+create_solution "sol0_ecc" "ecc" "directives_sol0.tcl"
+
+
+
+# Add custom files
+add_files -tb ../projects/ecc/input_files/landscape.jpg
+add_files -tb ../projects/ecc/input_files/room.jpg
