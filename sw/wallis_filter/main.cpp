014ca9ed255689d3bf104eb80b0835d08f4438e1
hargorin <janstocker@gmx.ch>
2018-06-19 16:29:06 +0200

    C code for benchmark

--- sw/wallis_filter/main.cpp
+++ sw/wallis_filter/main.cpp
@@ -15,28 +15,30 @@
 using namespace cv;
 using namespace std;
 
-void printUsage () {
-    printf("wallis_filter: Image content enhancement\n");
-    printf("    usage: ./wallis_filter input_image output_image [-s]\n");
-    printf("        input_image    	Input image for content enhancement\n");
-    printf("        output_image   	Image file to write. Extension can be tiff, png, ...\n");
-    printf("        -s          	Show image\n");
-    printf("                              \n");
-    printf("    example: ./wallis_filter input_files/landscape.jpg wallis.jpg -s\n");
-}
 
+// ****************************************************************************
+// defines
+#define G_MEAN      127     // Mean of the input image
+#define G_VAR       3600    // Standard Deviation for the input image
+#define CONTRAST    0.82    // Contrast expansion factor
+#define BRIGHTNESS  0.49    // Brightness forcing factor
+
+#define WIN_LENGTH  21  // Between 11 and 41 (depends on camera resolution)
+#define WIN_SIZE    (WIN_LENGTH * WIN_LENGTH)
+
+
+// ****************************************************************************
+// Declerations
+void printUsage ();
+
+
+// ****************************************************************************
+// Main
+// ****************************************************************************
 int main(int argc, const char * argv[]) {
 
-	// Declerations
-    const int MEAN = 127;			// Mean of the input image
-    const int STD = 60;				// Standard Deviation for the input image
-    const float CONTRAST = 1.6;		// Contrast expansion factor
-    const float BRIGHTNESS = 0.6;		// Brightness forcing factor
-
-    const int LOC_LENGTH = 25;
-    const int LOC_WIDTH = 25;
-
-
+    // ************************************************************************
+    // Read input arguments
     if(argc < 3)
     {
         printf("ERROR: not enough arguments\n");
@@ -51,112 +53,97 @@ int main(int argc, const char * argv[]) {
         return 2;
     }
 
-
-
-    // ************************************************************
-    // Read input arguments
     const char * infile = argv[1];
     const char * outfile = argv[2];
 
 
+    // ************************************************************************
 	// Read input image
-    Mat img = imread(infile, IMREAD_GRAYSCALE);
-    Mat w_img(img.rows, img.cols, CV_8UC1, Scalar(0));
-    int img_size = img.rows * img.cols;
-  
+    Mat src_img = imread(infile, IMREAD_GRAYSCALE);
+    if (!src_img.data) {
+        printf("***********************************************************\n");
+        printf("    ERROR: could not open or find the input image!\n");
+        printf("***********************************************************\n");
+        return 1;
+    }
 
-    int loc_mean = 0;
-    int loc_std = 0;
+    uint16_t img_width = src_img.cols;
+    uint16_t img_height = src_img.rows;
+    uint16_t g_height = (img_height - WIN_LENGTH + 1);
+    uint16_t g_width = (img_width - WIN_LENGTH + 1);
 
-    int mean_y = 0;
-    int std_y = 0;
-    int wal_y = 0;
-    int loop_y = 0;
-    int loop_x = 0;
-    int mean_x = 0;
-
-    for(int i = 0; i < img.rows; ) {
-    	loop_x = 0;
-
-		if((i + LOC_WIDTH) >= img.rows) {
-			loop_y += img.rows - i; 
-		}
-		else {
-			loop_y += LOC_WIDTH;
-		}
-
-    	for (int j = 0; j < img.cols; ) {
-    		mean_y = i;
-    		std_y = i;
-    		wal_y = i;
-
-    		if((j + LOC_LENGTH) >= img.cols) {
-    			loop_x += img.cols - j; 
-    		}
-    		else {
-    			loop_x += LOC_LENGTH;
-    		}
+    Mat w_img = Mat(g_height, g_width, CV_8UC1, Scalar(0));
 
 
-		    // ************************************************************
-		    // Calculate local mean
-		    for (; mean_y < loop_y; mean_y++) {
-		        for (mean_x = j; mean_x < loop_x; mean_x++) {
-		           	loc_mean += img.at<uchar>(Point(mean_x, mean_y));
-		        }
-		    }
-		    loc_mean = loc_mean / img_size;
+    // ************************************************************************
+    // Initialization
+    // ************************************************************************
+    static uint32_t sum_Pixel;
+    static uint32_t sum_Pixel2;
+
+    // Read data and calculate mean
+    sum_Pixel = 0;
+    sum_Pixel2 = 0;
+
+    for(uint16_t y = 0; y < g_height; y++) {
+
+        for(uint16_t i = 0; i < WIN_SIZE; i++) {
+            sum_Pixel += src_img.at<uint8_t>(Point(y + i, (y + y_offset)));
+            sum_Pixel2 += (src_img.at<uint8_t>(Point(x + x_offset, (y + y_offset))) * src_img.at<uint8_t>(Point(x + x_offset, (y + y_offset))));
+            sum_Pixel2 += tmp_pow;
+        }
 
 
 
-		    // ************************************************************
-		    // Calculate local standard deviation
-		    float tmp = 0;
-		    double var = 0;
-		    
-		    for (; std_y < loop_y; std_y++) {
-		        for (int std_x = j; std_x < loop_x; std_x++) {
-		           	tmp = img.at<uchar>(Point(std_x, std_y));
-		           	var += pow((tmp - loc_mean), 2);
-		        }
-		    }
-		    var = var / (img_size - 1);
-		    loc_std = sqrt(var);
 
+       for(uint16_t x = 0; x < g_width; x++) {
 
-
-		    // ************************************************************
-		    // Wallis filtering
-		    tmp = 0;
-
-		    for (; wal_y < loop_y; wal_y++) {
-		        for (int wal_x = j; wal_x < loop_x; wal_x++) {
-		        	tmp = STD * (img.at<uchar>(Point(wal_x, wal_y)) - loc_mean);
-		        	tmp = tmp / (loc_std + CONTRAST);
-		        	tmp = tmp + (MEAN * BRIGHTNESS);
-		        	tmp = tmp + (loc_mean * (1 - BRIGHTNESS));
-		        	w_img.at<uchar>(Point(wal_x, wal_y)) = tmp;
-		        }
-		    }
-    		
-
-
-
-    		j += LOC_LENGTH;
-    	}
-    	i += LOC_WIDTH;
+        } 
     }
 
 
 
 
 
-    // Output
+  
+
+    
+
+
+
+
+
+    // ************************************************************************
+    // Show Image
     if (argc == 4 && strcmp(argv[3], "-s") == 0)
     {
         //imshow( "Original", img ); 
         imshow( "Wallis Filter", w_img );                   
         waitKey(0);  
     }
+
     return 0;
+}
+
+
+void printUsage () {
+    printf("wallis_filter: Image content enhancement\n");
+    printf("    usage: ./wallis_filter input_image output_image [-s]\n");
+    printf("        input_image     Input image for content enhancement\n");
+    printf("        output_image    Image file to write. Extension can be tiff, png, ...\n");
+    printf("        -s              Show image\n");
+    printf("                              \n");
+    printf("    example: ./wallis_filter input_files/landscape.jpg wallis.jpg -s\n");
+}
+
+
+/*
+ * Calculate the mean
+ */
+uint8_t Cal_Mean(uint32_t sum_Pixel) {
+    uint32_t mean;
+
+    mean = sum_Pixel / WIN_SIZE;
+
+    return (uint8_t)mean;
 }
