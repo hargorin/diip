54b20a9d67b3d757b3eac749c71c13bb39b0584f
hargorin <janstocker@gmx.ch>
2018-06-22 16:33:19 +0200

    matlab code is working

--- sw/matlab/wallis/wallis_filter.m
+++ sw/matlab/wallis/wallis_filter.m
@@ -2,13 +2,13 @@ function wallis_filter()
     %*******************************************************************************************************
     %# read image
     %I = imread('room.jpg');
-    %O=imread('landscape.jpg');
-    %W=imread('landscape.jpg');
+    O=imread('mountain_small.tif');
+    W=imread('mountain_small.tif');
     
-    [O,map1]=imread('mountain.jpg');
-    O = rgb2gray( [O,map1]);
-    [W,map2]=imread('mountain.jpg');
-    W = rgb2gray( [W,map2]);
+    %[O,map1]=imread('room.jpg');
+    %O = rgb2gray( [O,map1]);
+    %[W,map2]=imread('room.jpg');
+    %W = rgb2gray( [W,map2]);
 
     
     %*******************************************************************************************************
@@ -42,7 +42,7 @@ function wallis_filter()
     
     % Textbox: WIN_SIZE
     uicontrol('Style','text', 'Position',[375 5 100 15], 'String','WIN_SIZE:');
-    tb_win = uicontrol('Style','edit', 'Position',[475 5 40 15], 'String','31', 'Callback',@tb_event);
+    tb_win = uicontrol('Style','edit', 'Position',[475 5 40 15], 'String','21', 'Callback',@tb_event);
     
     hFig.Visible = 'on';
     
@@ -94,24 +94,29 @@ function wallis_filter()
              [n_mean, n_std] = cal_mean_std(O);
              cal_wallis(n_mean, n_std);
          end
-         
     end
 
     %# Wallis Filter calculation
     function cal_wallis(n_mean, n_std)
         g_mean = str2num(get(tb_mean,'String'));
         g_std = str2num(get(tb_std,'String'));
-        %b = str2num(get(txt_b,'String'));
-        %c = str2num(get(txt_c,'String'));
-        b = 0.49;
-        c = 0.82;
+        b = str2num(get(txt_b,'String'));
+        c = str2num(get(txt_c,'String'));
         
         [rows cols]=size(O);
         
         for x = 1:rows
             for y = 1:cols
                 dbg = (((double(O(x,y)) - n_mean(x,y)) * c*g_std^2) / (c*n_std(x,y)^2+(1-c)*g_std^2));
-                W(x,y) = dbg + (b*g_mean + ((1-b)*n_mean(x,y)));
+                pix = dbg + (b*g_mean + ((1-b)*n_mean(x,y)));
+                
+                if pix >= 255
+                    W(x,y) = 255;
+                elseif pix <= 0
+                    W(x,y) = 0;
+                else
+                    W(x,y) = pix;
+                end
             end
         end
         
@@ -123,8 +128,6 @@ function wallis_filter()
         subplot(2,2,4);
         imhist(W);
         title('Histogram: Wallis Filter'); 
-        
-        disp('done wallis');
     end
 
     %# Calculate: Mean & Standard Deviation
