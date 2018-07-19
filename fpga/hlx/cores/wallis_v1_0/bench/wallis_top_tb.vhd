c129f14d72ed760e0c3793344fa7fbbddfa2bbe4
hargorin <janstocker@gmx.ch>
2018-07-19 16:19:22 +0200

    wallis top with errors

--- fpga/hlx/cores/wallis_v1_0/bench/wallis_top_tb.vhd
+++ fpga/hlx/cores/wallis_v1_0/bench/wallis_top_tb.vhd
@@ -1,18 +1,18 @@
 -------------------------------------------------------------------------------
--- Title       : <Title Block>
--- Project     : Default Project Name
+-- Title       : Wallis Filter Top
+-- Project     : Wallis Filter
 -------------------------------------------------------------------------------
--- File        : uft_top_tb.vhd
--- Author      : Noah Huetter <noahhuetter@gmail.com>
+-- File        : wallis_top_tb.vhd
+-- Author      : Jan Stocker (jan.stocker@students.fhnw.ch)
 -- Company     : User Company Name
--- Created     : Tue Nov 28 09:21:20 2017
--- Last update : Wed May  9 15:18:51 2018
+-- Created     : Thu Jul 19 16:06:12 2018
+-- Last update : Thu Jul 19 16:13:34 2018
 -- Platform    : Default Part Number
 -- Standard    : <VHDL-2008 | VHDL-2002 | VHDL-1993 | VHDL-1987>
 -------------------------------------------------------------------------------
--- Copyright (c) 2017 User Company Name
+-- Copyright (c) 2018 FHNW
 -------------------------------------------------------------------------------
--- Description: 
+-- Description: Testbench for the Wallis filter
 -------------------------------------------------------------------------------
 -- Revisions:  Revisions and documentation are controlled by
 -- the revision control system (RCS).  The RCS should be consulted
@@ -28,27 +28,57 @@ use ieee.std_logic_textio.all;
 -----------------------------------------------------------
 
 entity wallis_top_tb is
-    generic (
-        CLOCK_FREQ          : integer := 125000000                         -- freq of data_in_clk -- needed to timout cntr
-        
-    );
+
 end entity wallis_top_tb;
 
 -----------------------------------------------------------
 
 architecture testbench of wallis_top_tb is
 
-    -- Testbench signals
-    signal clk     :    std_logic;
-    signal rst_n   :    std_logic;
+	-- Testbench DUT generics as constants
+    constant WIN_SIZE     : positive              := 21*21;
+    constant M_IN_WIDTH   : positive              := 8;
+    constant M_OUT_WIDTH  : positive              := 17;
+    constant V_IN_WIDTH   : positive              := 16;
+    constant V_OUT_WIDTH  : positive              := 25;
+    constant REC_WIN_SIZE : unsigned(14 downto 0) := "100101001001101";
 
- 
-    constant clk_period : time := 8 ns;
+    constant DELAYTIME    : natural 			  := 10;
+
+	-- Testbench DUT ports as signals
+    signal clk                    : std_logic;
+    signal rst_n                  : std_logic;
+    signal wa_par_c_gvar          : std_logic_vector (19 downto 0);
+    signal wa_par_c               : std_logic_vector (5 downto 0);
+    signal wa_par_ci_gvar         : std_logic_vector (19 downto 0);
+    signal wa_par_b_gmean         : std_logic_vector (13 downto 0);
+    signal wa_par_bi              : std_logic_vector (5 downto 0);
+    signal i_axis_tlast           : std_logic;
+    signal i_axis_tready          : std_logic;
+    signal i_axis_tvalid          : std_logic;
+    signal i_axis_tdata           : std_logic_vector(7 downto 0);
+    signal o_axis_tlast           : std_logic;
+    signal o_axis_tready          : std_logic;
+    signal o_axis_tvalid          : std_logic;
+    signal o_axis_tdata           : std_logic_vector(7 downto 0);
+    signal m_axis_dividend_tvalid : std_logic;
+    signal m_axis_dividend_tready : std_logic;
+    signal m_axis_dividend_tdata  : std_logic_vector(23 downto 0);
+    signal m_axis_divisor_tvalid  : std_logic;
+    signal m_axis_divisor_tready  : std_logic;
+    signal m_axis_divisor_tdata   : std_logic_vector(15 downto 0);
+    signal s_axis_dout_tvalid     : std_logic;
+    signal s_axis_dout_tready     : std_logic;
+    signal s_axis_dout_tdata      : std_logic_vector(31 downto 0);
+
+	-- Other constants
+    constant clk_period : time := 10 ns;
     signal stop_sim  : std_logic := '0';
+
 begin
-    -----------------------------------------------------------
-    -- Clocks and Reset
-    -----------------------------------------------------------
+	-----------------------------------------------------------
+	-- Clocks and Reset
+	-----------------------------------------------------------
     CLK_GEN : process
     begin
         clk <= '1';
@@ -68,23 +98,65 @@ begin
         wait;
     end process RESET_GEN;
 
+	-----------------------------------------------------------
+	-- Testbench Stimulus
+	-----------------------------------------------------------
 
-    -----------------------------------------------------------
-    -- Testbench Stimulus
-    -----------------------------------------------------------
-    p_sim : process
-        procedure waitfor ( t : in natural ) is
-        begin
-            wait for t*clk_period;
-            wait until rising_edge(clk);
-        end procedure waitfor;
+	-----------------------------------------------------------
+	-- Entity Under Test
+	-----------------------------------------------------------
+    DUT : entity work.wallis_top
+        generic map (
+            WIN_SIZE     => WIN_SIZE,
+            M_IN_WIDTH   => M_IN_WIDTH,
+            M_OUT_WIDTH  => M_OUT_WIDTH,
+            V_IN_WIDTH   => V_IN_WIDTH,
+            V_OUT_WIDTH  => V_OUT_WIDTH,
+            REC_WIN_SIZE => REC_WIN_SIZE
+        )
+        port map (
+            clk                    => clk,
+            rst_n                  => rst_n,
+            wa_par_c_gvar          => wa_par_c_gvar,
+            wa_par_c               => wa_par_c,
+            wa_par_ci_gvar         => wa_par_ci_gvar,
+            wa_par_b_gmean         => wa_par_b_gmean,
+            wa_par_bi              => wa_par_bi,
+            i_axis_tlast           => i_axis_tlast,
+            i_axis_tready          => i_axis_tready,
+            i_axis_tvalid          => i_axis_tvalid,
+            i_axis_tdata           => i_axis_tdata,
+            o_axis_tlast           => o_axis_tlast,
+            o_axis_tready          => o_axis_tready,
+            o_axis_tvalid          => o_axis_tvalid,
+            o_axis_tdata           => o_axis_tdata,
+            m_axis_dividend_tvalid => m_axis_dividend_tvalid,
+            m_axis_dividend_tready => m_axis_dividend_tready,
+            m_axis_dividend_tdata  => m_axis_dividend_tdata,
+            m_axis_divisor_tvalid  => m_axis_divisor_tvalid,
+            m_axis_divisor_tready  => m_axis_divisor_tready,
+            m_axis_divisor_tdata   => m_axis_divisor_tdata,
+            s_axis_dout_tvalid     => s_axis_dout_tvalid,
+            s_axis_dout_tready     => s_axis_dout_tready,
+            s_axis_dout_tdata      => s_axis_dout_tdata
+        );
 
-    begin
-        waitfor(30);
-
-        stop_sim <= '1';
-        wait;
-    end process p_sim;
-
+    div_model : entity work.div_model
+        generic map (
+            DELAYTIME => DELAYTIME
+        )
+        port map (
+            clk            => clk,
+            rst_n          => rst_n,
+            i1_axis_tready => m_axis_dividend_tready,
+            i1_axis_tvalid => m_axis_dividend_tvalid,
+            i1_axis_tdata  => m_axis_dividend_tdata,
+            i2_axis_tready => m_axis_divisor_tready,
+            i2_axis_tvalid => m_axis_divisor_tvalid,
+            i2_axis_tdata  => m_axis_divisor_tdata,
+            o_axis_tready  => s_axis_dout_tready,
+            o_axis_tvalid  => s_axis_dout_tvalid,
+            o_axis_tdata   => s_axis_dout_tdata
+        );        
 
 end architecture testbench;
--- fpga/hlx/cores/wallis_v1_0/hdl/wallis_top.vhd
+++ fpga/hlx/cores/wallis_v1_0/hdl/wallis_top.vhd
@@ -1,18 +1,18 @@
 -------------------------------------------------------------------------------
--- Title       : UFT Top Module
--- Project     : Default Project Name
+-- Title       : Wallis Filter Top
+-- Project     : Wallis Filter
 -------------------------------------------------------------------------------
--- File        : uft_top.vhd
--- Author      : User Name <user.email@user.company.com>
+-- File        : wallis_top.vhd
+-- Author      : Jan Stocker (jan.stocker@students.fhnw.ch)
 -- Company     : User Company Name
--- Created     : Wed Nov 22 15:53:25 2017
--- Last update : Wed May  9 15:23:24 2018
+-- Created     : Thu Jul 19 13:57:22 2018
+-- Last update : Thu Jul 19 16:12:30 2018
 -- Platform    : Default Part Number
 -- Standard    : <VHDL-2008 | VHDL-2002 | VHDL-1993 | VHDL-1987>
 -------------------------------------------------------------------------------
--- Copyright (c) 2017 User Company Name
+-- Copyright (c) 2018 FHNW
 -------------------------------------------------------------------------------
--- Description: UDP File Transfer top module, combines transmitter and receiver
+-- Description: Top for the wallis filter
 -------------------------------------------------------------------------------
 -- Revisions:  Revisions and documentation are controlled by
 -- the revision control system (RCS).  The RCS should be consulted
@@ -26,21 +26,252 @@ USE IEEE.NUMERIC_STD.ALL;
 
 
 entity wallis_top is
-    --generic (
-
-    --);
+    generic (
+    	WIN_SIZE 	  : positive 			  := 21*21;
+        M_IN_WIDTH    : positive              := 8;
+        M_OUT_WIDTH   : positive              := 17;
+        V_IN_WIDTH    : positive              := 16;
+        V_OUT_WIDTH   : positive              := 25;
+        REC_WIN_SIZE  : unsigned(14 downto 0) := "100101001001101"
+    );
     port (
         -- clk and reset
         ------------------------------------------------------------------------
-        clk     : in    std_logic;
-        rst_n   : in    std_logic
+        clk     				: in    std_logic;
+        rst_n   				: in    std_logic;
 
+        -- control
+        ------------------------------------------------------------------------
+        wa_par_c_gvar 			: in std_logic_vector (19 downto 0);
+        wa_par_c 				: in std_logic_vector (5  downto 0);
+        wa_par_ci_gvar 			: in std_logic_vector (19 downto 0);
+        wa_par_b_gmean 			: in std_logic_vector (13 downto 0);
+        wa_par_bi	 			: in std_logic_vector (5  downto 0);
+
+        -- input stream
+        ------------------------------------------------------------------------
+        i_axis_tlast            : in std_logic;
+        i_axis_tready           : out std_logic;
+        i_axis_tvalid           : in std_logic;
+        i_axis_tdata            : in std_logic_vector(7 downto 0);
+        
+        -- output stream
+        ------------------------------------------------------------------------
+        o_axis_tlast            : out std_logic;
+        o_axis_tready           : in std_logic;
+        o_axis_tvalid           : out std_logic;
+        o_axis_tdata            : out std_logic_vector(7 downto 0);
+
+        -- divisor core
+        ------------------------------------------------------------------------
+        m_axis_dividend_tvalid : out	std_logic;
+        m_axis_dividend_tready : in 	std_logic;
+        m_axis_dividend_tdata  : out	std_logic_vector(23 downto 0);
+        m_axis_divisor_tvalid  : out	std_logic;
+        m_axis_divisor_tready  : in 	std_logic;
+        m_axis_divisor_tdata   : out	std_logic_vector(15 downto 0);
+        s_axis_dout_tvalid 	   : in 	std_logic;
+        s_axis_dout_tready 	   : out 	std_logic;
+        s_axis_dout_tdata 	   : in 	std_logic_vector(31 downto 0)
     );
 end entity wallis_top;
 
 architecture structural of wallis_top is
 
-begin
-  
+    component mean_var is
+        generic (
+            WIN_SIZE    : positive              := 21*21;
+            M_IN_WIDTH  : positive              := 8;
+            M_OUT_WIDTH : positive              := 17;
+            V_IN_WIDTH  : positive              := 16;
+            V_OUT_WIDTH : positive              := 25;
+            FIX_N       : unsigned(14 downto 0) := "100101001001101"
+        );
+        port (
+            clk     : in  std_logic;
+            rst_n   : in  std_logic;
+            inData  : in  std_logic_vector(7 downto 0);
+            outMean : out std_logic_vector(7 downto 0);
+            outVar  : out std_logic_vector(13 downto 0);
+            valid   : out std_logic;
+            en      : in  std_logic;
+            clear   : in  std_logic
+        );
+    end component mean_var;
 
+    component wallis_filter is
+        port (
+            clk                    : in  std_logic;
+            rst_n                  : in  std_logic;
+            pixel                  : in  std_logic_vector(7 downto 0);
+            n_mean                 : in  std_logic_vector(7 downto 0);
+            n_var                  : in  std_logic_vector(13 downto 0);
+            par_c_gvar             : in  std_logic_vector(19 downto 0);
+            par_ci_gvar            : in  std_logic_vector(19 downto 0);
+            par_c                  : in  std_logic_vector(5 downto 0);
+            par_b_gmean            : in  std_logic_vector(13 downto 0);
+            par_bi                 : in  std_logic_vector(5 downto 0);
+            wallis                 : out std_logic_vector(7 downto 0);
+            m_axis_dividend_tvalid : out std_logic;
+            m_axis_dividend_tready : in  std_logic;
+            m_axis_dividend_tdata  : out std_logic_vector(23 downto 0);
+            m_axis_divisor_tvalid  : out std_logic;
+            m_axis_divisor_tready  : in  std_logic;
+            m_axis_divisor_tdata   : out std_logic_vector(15 downto 0);
+            s_axis_dout_tvalid     : in  std_logic;
+            s_axis_dout_tready     : out std_logic;
+            s_axis_dout_tdata      : in  std_logic_vector(31 downto 0);
+            valid                  : out std_logic;
+            en                     : in  std_logic;
+            clear                  : in  std_logic
+        );
+    end component wallis_filter;    
+
+    -- Signal Components
+    signal n_mean 			: std_logic_vector(7 downto 0);
+    signal n_var  			: std_logic_vector(13 downto 0);
+    signal valid_mean_var 	: std_logic;
+    signal clear  			: std_logic;
+    signal pi_latch 		: std_logic_vector(7 downto 0);
+    signal o_axis_tvalid_i 	: std_logic;
+
+    -- Other Signals
+    signal init 			: boolean := true;
+    signal ctr				: natural range 0 to WIN_SIZE;
+    signal o_axis_tlast_i 	: std_logic;
+
+begin
+	i_axis_tready <= '1';
+	o_axis_tvalid <= o_axis_tvalid_i;
+	o_axis_tlast <= o_axis_tlast_i;
+
+	-----------------------------------------------------------
+	-- Counter to catch the pixel in the middle of the 
+	-- neighborhood
+	-----------------------------------------------------------
+	p_pi_latch : process(clk) is
+	-----------------------------------------------------------
+	begin
+		if rising_edge(clk) then
+			if (rst_n = '0') then
+				init <= true;
+				ctr <= 0;
+			else
+				if (i_axis_tlast = '1') and (i_axis_tvalid = '1') then
+					init <= true;
+					ctr <= 0;
+				else
+					if (i_axis_tvalid = '1') then
+						if (ctr = 440) then
+							init <= false;
+							ctr <= 0;
+						elsif (ctr = 220) and init then
+							pi_latch <= i_axis_tdata;
+						elsif (ctr = 10) and not init then
+							pi_latch <= i_axis_tdata;
+							ctr <= 0;
+						else		
+							ctr <= ctr + 1;	
+						end if;
+					end if;
+				end if;
+			end if;
+		end if;
+	end process; -- p_pi_latch
+	-----------------------------------------------------------
+
+	-----------------------------------------------------------
+	-- Sets tlast on output if last pixel on input was read
+	-- and clears tlast if tlast was read
+	-----------------------------------------------------------
+	p_tlast : process(clk) is
+	-----------------------------------------------------------
+	begin
+		if rising_edge(clk) then
+			if (rst_n = '0') then
+				o_axis_tlast <= '0';
+			else
+				-- sets tlast
+				if (i_axis_tlast = '1') and (i_axis_tvalid = '1') then
+					o_axis_tlast <= '1';
+				end if;
+
+				-- clear tlast
+				if (o_axis_tvalid_i <= '1') and (o_axis_tready = '1') then
+					o_axis_tlast <= '0';
+				end if;
+			end if;
+		end if;
+	end process; -- p_tlast
+	-----------------------------------------------------------
+
+	-----------------------------------------------------------
+	-- Sets clear if tvalid from output and tlast and tready
+	-- are set
+	-----------------------------------------------------------
+	p_clear : process(clk) is
+	-----------------------------------------------------------
+	begin
+		if rising_edge(clk) then
+			if (rst_n = '0') then
+				clear <= '0';
+			else
+				-- sets clear
+				if (o_axis_tvalid_i = '1') and (o_axis_tready = '1') and (o_axis_tlast <= '1') then
+					clear <= '1';
+				else
+					clear <= '0';
+				end if;
+			end if;
+		end if;
+	end process; -- p_clear
+	-----------------------------------------------------------
+
+	
+    mean_var : mean_var
+        generic map (
+            WIN_SIZE    => WIN_SIZE,
+            M_IN_WIDTH  => M_IN_WIDTH,
+            M_OUT_WIDTH => M_OUT_WIDTH,
+            V_IN_WIDTH  => V_IN_WIDTH,
+            V_OUT_WIDTH => V_OUT_WIDTH,
+            FIX_N       => REC_WIN_SIZE
+        )
+        port map (
+            clk     => clk,
+            rst_n   => rst_n,
+            inData  => i_axis_tdata,
+            outMean => n_mean,
+            outVar  => n_var,
+            valid   => valid_mean_var,
+            en      => i_axis_tvalid,
+            clear   => clear
+        );
+
+    wallis_filter : wallis_filter
+        port map (
+            clk                    => clk,
+            rst_n                  => rst_n,
+            pixel                  => pi_latch,
+            n_mean                 => n_mean,
+            n_var                  => n_var,
+            par_c_gvar             => wa_par_c_gvar,
+            par_ci_gvar            => wa_par_ci_gvar,
+            par_c                  => wa_par_c,
+            par_b_gmean            => wa_par_b_gmean,
+            par_bi                 => wa_par_bi,
+            wallis                 => o_axis_tdata,
+            m_axis_dividend_tvalid => m_axis_dividend_tvalid,
+            m_axis_dividend_tready => m_axis_dividend_tready,
+            m_axis_dividend_tdata  => m_axis_dividend_tdata,
+            m_axis_divisor_tvalid  => m_axis_divisor_tvalid,
+            m_axis_divisor_tready  => m_axis_divisor_tready,
+            m_axis_divisor_tdata   => m_axis_divisor_tdata,
+            s_axis_dout_tvalid     => s_axis_dout_tvalid,
+            s_axis_dout_tready     => s_axis_dout_tready,
+            s_axis_dout_tdata      => s_axis_dout_tdata,
+            valid                  => o_axis_tvalid_i,
+            en                     => valid_mean_var,
+            clear                  => clear
+        );  
 end architecture structural;
